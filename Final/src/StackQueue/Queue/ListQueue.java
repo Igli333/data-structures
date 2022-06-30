8c95e8ff022d6ba8c0cfd0baaafbf56878918737
Igli Balla <igli.balla@3i-solutions.net>
2022-06-30 11:25:58 +0200

    ListQueue Implementation

--- Final/src/StackQueue/Queue/ListQueue.java
+++ Final/src/StackQueue/Queue/ListQueue.java
@@ -1,4 +1,33 @@
 package StackQueue.Queue;
 
-public class ListQueue {
+import LinkedList.Singly.ListNode;
+
+public class ListQueue<AnyType> {
+    private ListNode<AnyType> start, end;
+
+    public ListQueue() {
+        start = end = null;
+    }
+
+    public boolean isEmpty() {
+        return start == null;
+    }
+
+    public void makeEmpty(){
+        start = end = null;
+    }
+
+    public void enqueue(AnyType x){
+        ListNode<AnyType> newNode = new ListNode<>(x);
+        end.next = newNode;
+        end = end.next;
+    }
+
+    public AnyType dequeue(){
+        ListNode<AnyType> old = start;
+        start = start.next;
+        return old.element;
+    }
+
+
 }
