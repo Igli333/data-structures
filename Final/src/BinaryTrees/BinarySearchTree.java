3fd7ebbe7d80b2c59bd699b76f5844db2fc459d7
Igli Balla <igliballa333@outlook.com>
2022-07-02 13:43:03 +0200

    BST

--- Final/src/BinaryTrees/BinarySearchTree.java
+++ Final/src/BinaryTrees/BinarySearchTree.java
@@ -51,6 +51,9 @@ public class BinarySearchTree<AnyType> {
     }
 
     public void insertFromArray(AnyType[] array, BinaryNode<AnyType> root) {
+        if (array.length == 0) {
+            return;
+        }
         int middle = array.length / 2;
         root.element = array[middle];
         AnyType[] arrayToPass1 = (AnyType[]) new Object[middle];
@@ -59,10 +62,90 @@ public class BinarySearchTree<AnyType> {
         }
 
         AnyType[] arrayToPass2 = (AnyType[]) new Object[middle];
-        for (int i = middle + 1; i < array.length; i++) {
-            arrayToPass2[i] = array[i];
+        for (int i = 0; i < array.length / 2; i++) {
+            arrayToPass2[i] = array[2 * i];
         }
         insertFromArray(arrayToPass1, root.left);
         insertFromArray(arrayToPass2, root.right);
+    }
+
+    public AnyType find(AnyType x, BinaryNode<AnyType> root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.element == x) {
+            return root.element;
+        }
+
+        if (root.left != null && (int) root.element > (int) x) {
+            return find(x, root.left);
+        } else if (root.right != null && (int) root.element < (int) x) {
+            return find(x, root.right);
+        }
+
+        return null;
+    }
+
+    private BinaryNode<AnyType> findMin(BinaryNode<AnyType> root){
+        if (root.left == null){
+            return root;
+        }
+
+        return findMin(root.left);
+    }
+
+    private void removeMin(BinaryNode<AnyType> root){
+        if (root.left.left == null){
+            root.left = root.left.right;
+            return;
+        }
+
+        removeMin(root.left);
+    }
+
+    private BinaryNode<AnyType> findMaxNode(BinaryNode<AnyType> root){
+        if (root.right == null){
+            return root;
+        }
+
+        return findMaxNode(root.left);
+    }
+
+    private void removeMax(BinaryNode<AnyType> root){
+        if (root.right.right == null){
+            root.right = root.right.left;
+            return;
+        }
+
+        removeMax(root.right);
+    }
+
+    public void remove(AnyType x, BinaryNode<AnyType> root){
+        if (root == null){
+            return;
+        }
+
+        if (root.left.element == x){
+            BinaryNode<AnyType> max = findMaxNode(root.left);
+            removeMax(root.left);
+            max.left = root.left.left;
+            max.right = root.left.right;
+            root.left = max;
+            return;
+        } else if (root.right.element == x){
+            BinaryNode<AnyType> min = findMin(root.right);
+            removeMin(root.right);
+            min.right = root.right.right;
+            min.left = root.right.left;
+            root.right = min;
+            return;
+        }
+
+        if ((int) root.element > (int) x) {
+            remove(x, root.left);
+        } else if ((int) root.element < (int) x) {
+            remove(x, root.right);
+        }
     }
 }
